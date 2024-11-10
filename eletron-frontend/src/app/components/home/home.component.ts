9b65a6f6858fe4e4ab987f57cf429106bf10c23d
luisrossim <luisrossim23@gmail.com>
2024-11-10 12:51:32 -0300

    test: leaflet map prod

--- eletron-frontend/src/app/components/home/home.component.ts
+++ eletron-frontend/src/app/components/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import L, { latLng, marker, tileLayer, icon } from 'leaflet';
+import { latLng, marker, tileLayer, icon } from 'leaflet';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonModule, CardModule, LeafletModule],
+  imports: [ButtonModule, CardModule, LeafletModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -21,17 +22,30 @@ export class HomeComponent implements OnInit {
 
   options = {
     layers: [
-      tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
+      tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: '&copy; <a href="https://carto.com/">CartoDB</a> contributors'
       })
     ],
     zoom: 15,
-    center: latLng(-18.707342062096952, -40.401385467076366),
+    center: latLng(-18.706542062096952, -40.401385467076366),
   };
 
   marker = marker([-18.707342062096952, -40.401385467076366], { icon: this.customIcon }).bindPopup(
-    '<div class="popup-content"><h4 class="mb-1">Eletrônica do Luis</h4><span class="flex gap-1"><i class="pi pi-star-fill text-yellow-400"></i><i class="pi pi-star-fill text-yellow-400"></i><i class="pi pi-star-fill text-yellow-400"></i><i class="pi pi-star-fill text-yellow-400"></i><i class="pi pi-star-half-fill text-yellow-400"></i></span></div>'
+    `<div class="popup-content">
+      <h4 class="mb-1">Eletrônica do Luis</h4>
+      <p class="flex text-yellow-400 mt-0 mb-2 gap-1">
+        <span class="mr-1">4,6</span>
+        <i class="pi pi-star-fill"></i>
+        <i class="pi pi-star-fill"></i>
+        <i class="pi pi-star-fill"></i>
+        <i class="pi pi-star-fill"></i>
+        <i class="pi pi-star-half-fill"></i>
+      </p>
+      <a href="https://maps.google.com/maps/dir//Eletr%C3%B4nica+do+Luis+R.+Esp%C3%ADrito+Santo,+121+-+Beira+Rio+Nova+Ven%C3%A9cia+-+ES+29830-000/@-18.707284,-40.4013411,16z/data=!4m5!4m4!1m0!1m2!1m1!1s0xb5db42e8029599:0x3edcf99222fca05a" target="_blank" class="no-underline" rel="noopener noreferrer">
+        Como chegar?
+      </a>
+    </div>`
   );
 
   constructor(){}
